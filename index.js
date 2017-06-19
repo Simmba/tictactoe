d41164b355ef352a7ceda3dd7e0e40208472882c
Simmba <simmern.kang@gmail.com>
2017-06-19 15:17:39 -0700

    promise broken

--- index.js
+++ index.js
@@ -1,52 +1,64 @@
 const readline = require('readline');
-var tictactoe = function(board) {
- var board = board  || [["-","-","-"],["-","-","-"],["-","-","-"]];
- board.forEach((elem)=>{
-   elem.forEach((dash)=>{
-     console.log(dash);
-   });
-   console.log("\n");
- });
- console.log("Place X");
- console.log("Give coordinates! E.g: 0,0");
- var response = readline();
- placePiece(response);
- checker;
- board.forEach((elem)=>{
-   elem.forEach((dash)=>{
-     console.log(dash);
-   });
-   console.log("\n");
- });
- console.log("Place O");
- console.log("Give coordinates! E.g: 0,0");
- checker;
- tictactoe(board);
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+var tictactoe = function (board) {
+  var board = board || [
+    ["-", "-", "-"],
+    ["-", "-", "-"],
+    ["-", "-", "-"]
+  ];
+  board.forEach((elem) => {
+    console.log(elem[0] + elem[1] + elem[2]);
+  });
+  askX().then(askO).then(tictactoe(board));
+  
+  
+  tictactoe(board);
 }
-var checker = function(board) {
-  for(var i = 0; i<board.length; i++){
-    if(board[i][0] === board[i][1] && board[i][0] === board[i][2]){
+var checker = function (board) {
+  for (var i = 0; i < board.length; i++) {
+    if (board[i][0] === board[i][1] && board[i][0] === board[i][2]) {
       console.log("Player" + board[i][0] + " wins!");
       break;
-    }else if(board[i][i] === board[i+1][i] && board[i][0] === board[i+2][i]){
+    } else if (board[i][i] === board[i + 1][i] && board[i][0] === board[i + 2][i]) {
       console.log("Player" + board[i][i] + " wins!");
       break;
-    }else if(board[0][0] === board[1][1] && board[0][0] === board[2][2]){
+    } else if (board[0][0] === board[1][1] && board[0][0] === board[2][2]) {
       console.log("Player" + board[i][i] + " wins!");
       break;
-    }else if(board[0][2] === board[1][1] && board[0][0] === board[2][0]){
+    } else if (board[0][2] === board[1][1] && board[0][0] === board[2][0]) {
       console.log("Player" + board[i][i] + " wins!");
       break;
     }
   }
 }
-var placePiece= function(str, char) {
+var placePiece = function (str, char) {
   var arr = str.split(",");
-  if(this.board[parseInt(arr[0])][parseInt(arr[1])] === "-"){
+  if (this.board[parseInt(arr[0])][parseInt(arr[1])] === "-") {
     this.board[parseInt(arr[0])][parseInt(arr[1])] = char;
-  }else{
+  } else {
     console.log("Invalid move. Please enter new coordinates");
     var resp = readline();
     placePiece(resp, char);
   }
 }
+var askX = function(){
+  rl.question("Place X. Give coordinates! E.g: 0,0 \n", (answer) => {
+    console.log(answer);
+    placePiece(answer);
+    checker;
+    board.forEach((elem) => {
+      console.log(elem[0] + elem[1] + elem[2]);
+    });
+  });
+}
+var askO = function () {
+  rl.question("Place O. Give coordinates! E.g: 0,0", (answer) => {
+    console.log(answer);
+    placePiece(answer);
+    checker;
+  });
+}
+tictactoe();
